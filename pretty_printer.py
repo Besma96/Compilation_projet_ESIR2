33099223fc4e9d95e71a7356ac02f349bd9514f0
barry <daoudabarry9@gmail.com>
2018-11-09 14:20:07 +0100

    ajout de dossiers

---
+++ pretty_printer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 23:31:02 2018
+
+@author: barry
+this program is to make a pretty_printer for the while langage
+Methode : 
+    enregistrer les différentes règles du langage dans des listes ou autre objets pour ensuite les utiliséees pour traiter notre fichier
+"""
+import sys
+
+
