60fc450f4b9862abb1c11edcdf2dac260f805341
jeutkt <44072927+jeutkt@users.noreply.github.com>
2018-11-23 10:18:59 +0100

    Add files via upload

--- src/OP.java
+++ src/OP.java
@@ -1,5 +1,11 @@
-package compilation;
+package org.xtext.compilateur.generator;
 
 public enum OP {
-		NOP
+		NOP,
+		//control structures
+		If,Else,
+		//Loop
+		For,While,Foreach,
+		//Affectation
+		Affect
 }
