76f270bf02141f18f3b5cc3e5e5134064a155fa7
Besma96 <43781192+Besma96@users.noreply.github.com>
2018-12-26 21:44:51 +0100

    nouvelle librairie

--- src/Wh_lib/WhLib.py
+++ src/Wh_lib/WhLib.py
@@ -9,174 +9,65 @@ class WhLib:
         pass
 
     def nil(self):
-        return self.cons_intermediaire(None,None)
+        return bt.binTree().cons(None, None)
 
-    def cons(self,queue):
-        L=[]
-        while not queue.empty():
-            L.append(queue.get())
+    def hd(self, tree):
+        return tree.head()
 
-        return WhLib.cons_list(self,L)
+    def tail(self, tree):
+        return tree.tail()
 
-    def list(self,queue):
-        L=[]
-        while not queue.empty():
-            L.append(queue.get())
-        
-        return WhLib.list_intermediaire(self,L)
-
-    def hd(self,X):
-        return X.head()
-
-    def tail(self,X):
-        return X.tail()
-        
-    #istrue
-    def isTrue(self,X): 
-        if X.node == "nil" :
+    def isTrue(self, tree):
+        if tree.node=="nil":
             return False
         return True
+        
+    
+    def isEqual(self, X, Y):
+        return X.isEqual(Y)
 
+    def cons(self, l=None, r=None):
+        if( l is None and isinstance(r,bt.binTree)):
+            return bt.binTree().cons_solo(r)
+        if( r is None and isinstance(l,bt.binTree)):
+            return bt.binTree().cons_solo(l)
+        else:
+            return bt.binTree().cons(l,r)
 
-    ##construction d'un bintree à partir d'un int
-    ## @param self : binTree
-    ## @param val : int
-    def intToBinTree(self,val):
-        res = bt.binTree()
-        res=WhLib.cons_intermediaire(self,"nil", "nil")
+    def intToBinTree(self, val):
+        res=bt.binTree()
+        res.cons("nil", "nil")
         if(val != 0):
             i=0
             while i<val:
-                res=WhLib.cons_intermediaire(self,"nil", res)
+                res.cons("nil", res)
                 i=i+1
         return res
-
-    ## évalue un binTree en int
-    def binTreeToInt(self,X):
-        if X.right is None:## cas où le fils droit est vide (en terme de int)
+    
+    def binTreeToInt(self, X):
+        if X.getRight() is None:
             return 0
         res = 0
-        tree=bt.binTree()
-        tree=X.right
-        while(tree!="nil"):
-            res =res+1
-            tree=tree.getRight()
+        tree = bt.binTree()
+        tree = X.getRight()
+        while not tree.isNil() :
+            res = res+1
+            tree = tree.getRight()
         return res
 
-    ## Permet de savoir si deux arbres sont égaux
-    def isEqual(self,X,Y):
-        if X.left==Y.left:
-            if X.right==Y.right:
-                return True
-            else:
-                return False
+    def toString(self,X) -> str:
+        if (not isinstance(X,bt.binTree)):
+            return str(X)
         else:
-            return False
+            return "(  " +WhLib.toString(self,X.getLeft())+" cons " + WhLib.toString(self,X.getRight()) + ")"
+    
+    def and_wh(self,X,Y):
+        return (WhLib.isTrue(self,X) and WhLib.isTrue(self,Y))
 
-    def isNotEqual(self,X,Y):
-        return not self.isEqual(X,Y)
+    def or_wh(self,X,Y):
+        tmp=WhLib().isTrue(X) or WhLib().isTrue(Y)
+        return tmp
 
-    def StringToBinTree(self,abc):
-        L=[]
-        for i in range (0,len(abc)):
-            L.append(abc[i])# on lit chaque caractère qu'on stock dans la chaine L
-        
-        return self.cons_list(L)
-
-    def BinTreeToStr(self,tree):
-        chaine=""
-        tl=True
-        while tl:
-            try : 
-                chaine+=tree.left
-                tree=tree.right
-            except:
-                tl=False
-        chaine+=tree
-        return chaine
-              
-    ###########################################################################################################################
-    ###########################################################################################################################
-    #fonction intermediaire servant aux fonctions plus haut
-    ###########################################################################################################################
-    ###########################################################################################################################
-
-    ## Constructiond d'un arbre binaire en fonction de deux paramètres d'entrer
-    ##Fonction centrale de la classe binTree car à la base de toute construction
-    ## param : l = fils gauche
-    ## param : r : fils droit
-    ##retour : pas de retour mais création d'un arbre
-    def cons_intermediaire(self,l,r):
-        tree=bt.binTree()
-        #cas ou on a <cons var nil nil> = <nil    > 
-        if ((l is None) & (r is None)):
-            tree.node = "nil"
-            tree.left=None
-            tree.right=None
-        else:	
-            tree.node = "cons"
-            tree.left = l
-            tree.right = r
-        return tree
-
-        
-    ## construire un arbre binaire à partir d'un arbre binaire revient à renvoyer cet arbre bianaire
-    ## cf tab de traduction
-    def cons_solo(self,X):
-        tree=bt.binTree()
-        tree.node=X.node
-        tree.left=X.left
-        tree.right=X.right
-        return tree
-
-    ## construction d’un binTree à partir d’une file (deque) de binTree
-    ## parametre : LISTE D'element 
-    ##retour :  l'arbre binaire crée à partir de la file d'elmeent de queue 
-    def cons_list(self,L) :
-        tree=bt.binTree()
-        bl=True
-        if (len(L)==0): # cas particulier où la liste est vide*
-            tree=WhLib.cons_intermediaire(self,None,None)
-        else:
-            if(len(L)==1):# sil il n'y a qu'un seul élément dans la liste on réalise un retourne le parametre d'entré
-                i=L.pop(len(L)-1)
-                try:
-                    tree=WhLib.cons_solo(self,i)
-                except:
-                    tree=WhLib.cons_intermediaire(self,None,i)
-            else:
-                if(len(L)>=2):# sinon on crée l'arbre de plus bas niveau puis on construit le reste non recursivement (pb python avec recursivité)
-                    tree=WhLib.cons_intermediaire(self,L.pop(len(L)-2),L.pop(len(L)-1))
-                    while bl:
-                        if (len(L)>=1):
-                            tree=WhLib.cons_intermediaire(self,L.pop(len(L)-1), tree)
-                        else:
-                            bl=False
-        return tree	
-
-    ## construction d’un binTree à partir d’une file (deque) de binTree représenatnt une liste
-    ## parametre : queue (liste d'element)
-    ##retour :  l'arbre binaire crée à partir de la file d'elmeent de queue 
-    def list_intermediaire(self,L) :
-        tree=bt.binTree()
-        bl=True
-        if (len(L)==0): # cas particulier où la liste est vide
-            print("null")
-            tree=WhLib.cons_intermediaire(self,None,None)
-        else:
-            tree=WhLib.cons_intermediaire(self,L.pop(len(L)-1), "nil")
-            while bl:
-                if (len(L)>=1):
-                    tree=WhLib.cons_intermediaire(self,L.pop(len(L)-1), tree)
-                else:
-                    bl=False
-        return tree
-
-
-
-
-
-
-
-
-
+    def not_wh(self,X):
+        tmp=not(WhLib().isTrue(X))
+        return tmp
---
+++ src/Wh_lib/binTree.py
@@ -0,0 +1,88 @@
+class binTree:
+    #constructeur
+    # #Le constructeur est vide car on construira l'arbre grace aux methode list, cons, etc...
+    # #Utilisation : appeler le constructeur de la class ebibTRee consiste seulement à initialier reserver la place pour l'arbre qu dsera crée après
+    def __init__(self):
+        self.node = None
+        self.left = None
+        self.right = None
+
+  #  def __init__(self, data, leftTree, rightTree):
+   #     self.node=data
+   #     if self.node == "nil":
+    #        self.left = None
+    #        self.right = None
+     #   else:
+      #      self.left = leftTree
+       #     self.right = rightTree
+
+        
+    #getteur
+    def getNode(self):
+        return self.node
+    
+    def getLeft(self):
+        return self.left
+    
+    def getRight(self):
+        return self.right
+        
+    #setteur
+    def setLeft(self, leftTree):
+        self.left = leftTree
+    
+    def setRight(self, rightTree):
+        self.right = rightTree
+    
+    def head(self):
+        if self.left is not None :
+            return self.left
+        return None
+    
+    def tail(self):
+        if self.right is not None:
+            return self.right
+        return None
+
+    def isTrue(self):
+        if self.node == "nil":
+            return False
+        return True
+        
+    def isNil(self):
+        b1 = (self.node=="nil") and (self.left is None) and (self.right is None)
+        b2 = (self == "nil")
+        return (b1 or b2)
+
+    def isEqual(self, X):
+        if self.right == X.right:
+            if self.left == X.left:
+                return True
+            else :
+                return False
+        else:
+            return False
+
+
+    ## construction d'un binTree à partir de deux paramètres fils droit et fils gauche
+    def cons(self, l, r):
+        tree = binTree()
+        if((l is None) & (r is None)):
+            tree.node = "nil"
+            tree.left = None
+            tree.right = None
+        else :
+            tree.node = "cons"
+            tree.left = l
+            tree.right = r
+        return tree
+
+    ## construction d'un binTree à partir d'un binTree passé en paramètre
+    def cons_solo(self, X):
+        self.node = X.node
+        self.left = X.left
+        self.right = X.right
+        return self
+
+
+    
