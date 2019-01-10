b5e3903257fa1e0833361832ed7c1791f2289eb6
safdab <daoudabarry9@gmail.com>
2019-01-10 17:07:38 +0100

    ghsdbhvnj

--- Wh_lib/sortie.py
+++ Wh_lib/sortie.py
@@ -13,8 +13,6 @@ def F0(inputP , outputP ) :
 	outParams =queue.Queue() #Queue de bintree
 	 #Variables utilisées dans le code While de la fonction : main
 	nil = bt.WhLib().cons(None, None)
-	C = bt.WhLib().cons(None, None)
-	S = bt.WhLib().cons(None, None)
 	X = bt.WhLib().cons(None, None)
 	Y = bt.WhLib().cons(None, None)
 	Z = bt.WhLib().cons(None, None)
@@ -22,31 +20,22 @@ def F0(inputP , outputP ) :
 	
 	 #Variables temporaires utilisées par le compiler pour la fonction : main
 	Var1 = bt.WhLib().cons(None, None)
-	Var2 = bt.WhLib().cons(None, None)
+	Var3 = bt.WhLib().cons(None, None)
 	Var4 = bt.WhLib().cons(None, None)
 	Var5 = bt.WhLib().cons(None, None)
-	Var7 = bt.WhLib().cons(None, None)
-	Var8 = bt.WhLib().cons(None, None)
-	Var9 = bt.WhLib().cons(None, None)
 	
 	X = inputP.get()
 	Y = inputP.get()
 	Z = inputP.get()
 	Var1 = bt.WhLib().cons(nil,nil)
-	Var2 = bt.WhLib().cons(nil,Var1)
-	for C in bt.WhLib().all_nodes(Var2) : 
-		Var4 = bt.WhLib().cons(nil,C)
+	for C in bt.WhLib().all_nodes(Var1) : 
+		Var3 = bt.WhLib().cons(Y,Z)
+		Var4 = bt.WhLib().cons(X,Var3)
 		Var5 = Var4
-		S = Var5
-	Var7 = bt.WhLib().cons(Y,Z)
-	Var8 = bt.WhLib().cons(X,Var7)
-	Var9 = Var8
-	L = Var9
+		L = Var5
 	outputP.put(X)
 	outputP.put(Y)
-	outputP.put(Z)
 	outputP.put(L)
-	outputP.put(S)
 
 
 ########## Partie main ############
@@ -75,26 +64,6 @@ else :
 F0(inParams, outParams)
 
  #Affichage des paramètres de sortie
-result = outParams.get()
-if not (result==True or result==False or result == None ) : 
-	resultInt = bt.WhLib().binTreeToInt(result) 
-	if resultInt < 250 : 
-		print(bt.WhLib().toString(result))
-	else : 
-		print("Arbre trop grand pour l'affichage")
-	print("Son Equivalent en entier : " , 	resultInt)
-else : 
-	print("Son Equivalent en boolean : " , 	result)
-result = outParams.get()
-if not (result==True or result==False or result == None ) : 
-	resultInt = bt.WhLib().binTreeToInt(result) 
-	if resultInt < 250 : 
-		print(bt.WhLib().toString(result))
-	else : 
-		print("Arbre trop grand pour l'affichage")
-	print("Son Equivalent en entier : " , 	resultInt)
-else : 
-	print("Son Equivalent en boolean : " , 	result)
 result = outParams.get()
 if not (result==True or result==False or result == None ) : 
 	resultInt = bt.WhLib().binTreeToInt(result) 
