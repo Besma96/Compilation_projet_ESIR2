84426ea25eb7e7c914e0a9947960f6cee4130b7e
safdab <daoudabarry9@gmail.com>
2019-01-12 18:43:39 +0100

    dernier jar

--- Wh_lib/sortie.py
+++ Wh_lib/sortie.py
@@ -5,37 +5,73 @@ import sys
 import queue as queue
 
  # Les symboles utilisés dans le programme while
-nil = bt.WhLib().cons(None, None)
+nil = bt.WhLib().cons()
 
 
 def F0(inputP , outputP ) : 
 	inParams = queue.Queue() #Queue de bintree
 	outParams =queue.Queue() #Queue de bintree
 	 #Variables utilisées dans le code While de la fonction : main
-	nil = bt.WhLib().cons(None, None)
-	X = bt.WhLib().cons(None, None)
-	Y = bt.WhLib().cons(None, None)
-	Z = bt.WhLib().cons(None, None)
-	L = bt.WhLib().cons(None, None)
+	Nil = bt.WhLib().cons()
+	nil = bt.WhLib().cons()
+	B = bt.WhLib().cons()
+	C = bt.WhLib().cons()
+	K = bt.WhLib().cons()
+	L = bt.WhLib().cons()
 	
 	 #Variables temporaires utilisées par le compiler pour la fonction : main
-	Var1 = bt.WhLib().cons(None, None)
-	Var3 = bt.WhLib().cons(None, None)
-	Var4 = bt.WhLib().cons(None, None)
-	Var5 = bt.WhLib().cons(None, None)
+	Var0 = bt.WhLib().cons()
+	Var12 = bt.WhLib().cons()
+	Var1 = bt.WhLib().cons()
+	Var2 = bt.WhLib().cons()
+	Var4 = bt.WhLib().cons()
+	Var5 = bt.WhLib().cons()
+	Var6 = bt.WhLib().cons()
+	Var7 = bt.WhLib().cons()
+	Var8 = bt.WhLib().cons()
+	Var9 = bt.WhLib().cons()
+	Var11 = bt.WhLib().cons()
 	
-	X = inputP.get()
-	Y = inputP.get()
-	Z = inputP.get()
-	Var1 = bt.WhLib().cons(nil,nil)
-	for C in bt.WhLib().all_nodes(Var1) : 
-		Var3 = bt.WhLib().cons(Y,Z)
-		Var4 = bt.WhLib().cons(X,Var3)
-		Var5 = Var4
-		L = Var5
-	outputP.put(X)
-	outputP.put(Y)
+	B = inputP.get()
+	C = inputP.get()
+	inParams.put(B)
+	inParams.put(C)
+	F1(inParams, outParams)
+	Var1 = outParams.get()
+	Var2 = outParams.get()
+	Var4 = bt.WhLib().cons(Var2,Nil)
+	Var5 = bt.WhLib().cons(Var1,Var4)
+	Var6 = bt.WhLib().cons("nil",Var5)
+	Var7 = Var6
+	L = Var7
+	inParams.put(B)
+	inParams.put(C)
+	F1(inParams, outParams)
+	Var8 = outParams.get()
+	Var9 = outParams.get()
+	Var11 = bt.WhLib().cons(Var8,Var9)
+	Var12 = Var11
+	K = Var12
+	outputP.put(B)
+	outputP.put(C)
 	outputP.put(L)
+	outputP.put(K)
+
+
+def F1(inputP , outputP ) : 
+	inParams = queue.Queue() #Queue de bintree
+	outParams =queue.Queue() #Queue de bintree
+	 #Variables utilisées dans le code While de la fonction : autre
+	B = bt.WhLib().cons()
+	C = bt.WhLib().cons()
+	
+	 #Variables temporaires utilisées par le compiler pour la fonction : autre
+	
+	B = inputP.get()
+	C = inputP.get()
+	bt.WhLib().nop()
+	outputP.put(B)
+	outputP.put(C)
 
 
 ########## Partie main ############
@@ -43,27 +79,31 @@ def F0(inputP , outputP ) :
 inParams = queue.Queue() #Queue de BinTree
 outParams = queue.Queue() #Queue de BinTree
 if(len(sys.argv) > 1) : 
-	X = bt.WhLib().intToBinTree(int(sys.argv[1]))
-	inParams.put(X)
+	B = bt.WhLib().intToBinTree(int(sys.argv[1]))
+	inParams.put(B)
 else : 
-	X = bt.WhLib().cons(None, None)
-	inParams.put(X)
+	B = bt.WhLib().cons()
+	inParams.put(B)
 if(len(sys.argv) > 2) : 
-	Y = bt.WhLib().intToBinTree(int(sys.argv[2]))
-	inParams.put(Y)
+	C = bt.WhLib().intToBinTree(int(sys.argv[2]))
+	inParams.put(C)
 else : 
-	Y = bt.WhLib().cons(None, None)
-	inParams.put(Y)
-if(len(sys.argv) > 3) : 
-	Z = bt.WhLib().intToBinTree(int(sys.argv[3]))
-	inParams.put(Z)
-else : 
-	Z = bt.WhLib().cons(None, None)
-	inParams.put(Z)
+	C = bt.WhLib().cons()
+	inParams.put(C)
 
 F0(inParams, outParams)
 
  #Affichage des paramètres de sortie
+result = outParams.get()
+if not (result==True or result==False or result == None ) : 
+	resultInt = bt.WhLib().binTreeToInt(result) 
+	if resultInt < 250 : 
+		print(bt.WhLib().toString(result))
+	else : 
+		print("Arbre trop grand pour l'affichage")
+	print("Son Equivalent en entier : " , 	resultInt)
+else : 
+	print("Son Equivalent en boolean : " , 	result)
 result = outParams.get()
 if not (result==True or result==False or result == None ) : 
 	resultInt = bt.WhLib().binTreeToInt(result) 
